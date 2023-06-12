0c3060bd281e75a2069e57326251b4c1d85fb453
Ayush Gupta <ayush.gupta@qsstechnosoft.com>
2023-06-12 15:00:09 +0530

    removed delete fruit file

--- src/api/graphql/mutations/FruitsStoreMutations/index.js
+++ src/api/graphql/mutations/FruitsStoreMutations/index.js
@@ -1,5 +1,4 @@
 const { storeFruitToFruitStorage } = require('./storeFruitToFruitStorage')
 const { removeFruitFromFruitStorage } = require('./removeFruitFromFruitStorage')
-// const { deleteFruitFromFruitStorage } = require('./deleteFruitFromFruitStorage');
 
 module.exports = { storeFruitToFruitStorage, removeFruitFromFruitStorage }
